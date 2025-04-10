5d2de4f56b7709f714c708f349af04d9a0310bc0
bsarkr <bsarkar@fordham.edu>
2025-04-10 11:55:41 -0400

    Initial Commit

---
+++ StudyBuddy/StudyBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyBuddyApp.swift
+//  StudyBuddy
+//
+//  Created by Bilash Sarkar on 4/10/25.
+//
+
+import SwiftUI
+
+@main
+struct StudyBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StudyBuddyTests/StudyBuddyTests.swift
@@ -0,0 +1,17 @@
+//
+//  StudyBuddyTests.swift
+//  StudyBuddyTests
+//
+//  Created by Bilash Sarkar on 4/10/25.
+//
+
+import Testing
+@testable import StudyBuddy
+
+struct StudyBuddyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
